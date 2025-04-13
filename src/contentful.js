794243bb1bd3f61d64434556205979b359a6002d
Konstantin <keynicorn@gmail.com>
2025-04-13 12:03:00 +0300

    Update contentful.js

--- src/contentful.js
+++ src/contentful.js
@@ -1,26 +1,3 @@
-*// import { createClient } from "contentful";
-
-const client = createClient({
-  space: "xwylc92nxbor",
-  accessToken: "J1EVqyVWHG_8GBJAixQERvcaValwywAhTZ4NZjcH_1U",
-});
-
-export const fetchProducts = async () => {
-  const response = await client.getEntries({
-    content_type: "product",
-  });
-
-  // Map the raw Contentful entries to clean objects
-  return response.items.map((item) => ({
-    id: item.sys.id,
-    name: item.fields.name,
-    price: parseFloat(item.fields.price),
-    image: item.fields.image?.fields?.file?.url || "",
-    description: item.fields.description || "",
-    // aroma: item.fields.aromaSelection || "", // Single-select dropdown value
-  }));
-}; //*
-
 import {createClient} from 
 "contentful";
 

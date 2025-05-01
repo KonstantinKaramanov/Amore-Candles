943ec6a884594a9492d2bd78f5e9140eb11e854a
Konstantin <keynicorn@gmail.com>
2025-05-01 12:19:40 +0300

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,31 +1,30 @@
 // src/components/ProductList.jsx
 import React, { useEffect, useState } from "react";
-import { fetchProducts } from "../utils/contentful";
 import ProductCard from "./ProductCard";
+import { fetchProducts } from "../contentful";
 
-const ProductList = ({ onCheckout, showFullDescription }) => {
+export default function ProductList({ featuredOnly = false, onCheckout }) {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
-    const load = async () => {
-      const data = await fetchProducts();
-      setProducts(data);
-    };
-    load();
+    (async () => {
+      const all = await fetchProducts();
+      setProducts(all);
+    })();
   }, []);
+
+  const display = featuredOnly
+    ? products.filter((p) => p.featured)
+    : products;
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {products.map((product) => (
+      {display.map((product) => (
         <ProductCard
           key={product.id}
           product={product}
-          showFullDescription={showFullDescription}
           onCheckout={onCheckout}
         />
       ))}
     </div>
   );
-};
-
-export default ProductList;
+}

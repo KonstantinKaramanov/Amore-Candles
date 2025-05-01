705f0dc1192778bd2f28ffb2ecdc1e14b00fd2cd
Konstantin <keynicorn@gmail.com>
2025-05-01 12:27:06 +0300

    Update contentful.js

--- src/utils/contentful.js
+++ src/utils/contentful.js
@@ -1,72 +1,26 @@
-
-// This file is responsible for fetching data from Contentful
+// src/contentful.js
 import { createClient } from "contentful";
-
 
 const client = createClient({
   space: "xwylc92nxbor",
   accessToken: "J1EVqyVWHG_8GBJAixQERvcaValwywAhTZ4NZjcH_1U",
 });
 
-// 🔁 Fetch all products
 export async function fetchProducts() {
   const entries = await client.getEntries({ content_type: "product" });
-
   return entries.items.map((item) => {
-    const imagesField = item.fields.image;
-
-    const imagesArray = Array.isArray(imagesField)
-      ? imagesField
-      : imagesField
-      ? [imagesField]
-      : [];
-
-    return {
-      id: item.sys.id,
-      name: item.fields.name,
-      description: item.fields.description,
-      price: item.fields.price,
-      images: imagesArray.map((img) => "https:" + img.fields.file.url),
-    };
-  });
-}
-
-// 🔍 Fetch a single product by ID
-export async function fetchProductById(id) {
-  const entry = await client.getEntry(id);
-
-  const imagesField = entry.fields.image;
-  const imagesArray = Array.isArray(imagesField)
-    ? imagesField
-    : imagesField
-    ? [imagesField]
-    : [];
-
-  return {
-    id: entry.sys.id,
-    name: entry.fields.name,
-    description: entry.fields.description,
-    price: entry.fields.price,
-    images: imagesArray.map((img) => "https:" + img.fields.file.url),
-  };
-}
-export async function fetchFeaturedProducts() {
-  const entries = await client.getEntries({
-    content_type: "product",
-    "fields.featured": true,
-  });
-
-  return entries.items.map((item) => {
-    const images = Array.isArray(item.fields.image)
+    const imgs = Array.isArray(item.fields.image)
       ? item.fields.image
-      : [item.fields.image];
-
+      : item.fields.image
+      ? [item.fields.image]
+      : [];
     return {
       id: item.sys.id,
       name: item.fields.name,
       description: item.fields.description,
       price: item.fields.price,
-      images: images.map((img) => "https:" + img.fields.file.url),
+      featured: item.fields.featured || false, // add this field in Contentful
+      images: imgs.map((i) => "https:" + i.fields.file.url),
     };
   });
 }
